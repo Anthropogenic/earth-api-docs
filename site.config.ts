908cf0e2322ae02031b2c6fe95519241b7a90fcd
William Felker <gndclouds@icloud.com>
2022-07-05 18:35:55 +0000

    added EathAPI Page URL

--- site.config.ts
+++ site.config.ts
@@ -2,12 +2,11 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'Earth-API-Docs-f612e44da33d44ada672295d3db45da2',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
-
   // basic site info (required)
   name: 'Next.js Notion Starter Kit',
   domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
